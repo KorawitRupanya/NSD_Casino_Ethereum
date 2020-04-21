95e86f275d953816356aff3fe231a3285e6966a4
Korwit <korawit.r@ku.th>
2020-04-21 14:53:24 +0700

    fix contracts and add mirgration files for deploy contracts

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,70 +1,100 @@
-pragma solidity 0.4.20;
+pragma solidity ^0.4.16;
+
 
 contract Casino {
-   address public owner;
-   uint256 public minimumBet;
-   uint256 public totalBet;
-   uint256 public numberOfBets;
-   uint256 public maxAmountOfBets = 100;
-   address[] public players;
+    address public owner;
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public numberOfBets;
+    uint256 public maximumBetsNr = 100;
+    address[] public players;
 
-   struct Player {
-      uint256 amountBet;
-      uint256 numberSelected;
-   }
-// The address of the player and => the user info
-   mapping(address => Player) public playerInfo;
+    struct Player {
+        uint256 amountBet;
+        uint256 numberSelected;
 
-   function Casino() public {
-      if(_minimumBet != 0 ) minimumBet = _minimumBet;
-      owner = msg.sender;
-   }
-   function kill() public {
-      if(msg.sender == owner) selfdestruct(owner);
-   }
+    }
 
-   function checkPlayerExists(address player) public constant returns(bool){
-      for(uint256 i = 0; i < players.length; i++){
-         if(players[i] == player) return true;
-      }
-      return false;
-   }
+    // The address of the player and => the user info
+    mapping(address => Player) public playerInfo;
 
-   // To bet for a number between 1 and 10 both inclusive
-   function bet(uint256 numberSelected) public payable {
-      require(!checkPlayerExists(msg.sender));
-      require(numberSelected >= 1 && numberSelected <= 10);
-      require(msg.value >= minimumBet);
+    function Casino(uint256 _minimumBet) public payable{
+        owner = msg.sender;
+        if (_minimumBet != 0) 
+            minimumBet = _minimumBet;
 
-      playerInfo[msg.sender].amountBet = msg.value;
-      playerInfo[msg.sender].numberSelected = numberSelected;
-      numberOfBets++;
-      players.push(msg.sender);
-      totalBet += msg.value;
+    }
 
-  // Generates a number between 1 and 10 that will be the winner
-   function generateNumberWinner() public {
-      uint256 numberGenerated = block.number % 10 + 1; // This isn't secure
-      distributePrizes(numberGenerated);
-      }
+    function() public payable {}
 
-   // Sends the corresponding ether to each winner depending on the total bets
-   function distributePrizes(uint256 numberWinner) public {
-      address[100] memory winners; // We have to create a temporary in memory array with fixed size
-      uint256 count = 0; // This is the count for the array of winners
-      for(uint256 i = 0; i < players.length; i++){
-         address playerAddress = players[i];
-         if(playerInfo[playerAddress].numberSelected == numberWinner){
-            winners[count] = playerAddress;
-            count++;
-         }
-         delete playerInfo[playerAddress]; // Delete all the players
-      }
-      players.length = 0; // Delete all the players array
-      uint256 winnerEtherAmount = totalBet / winners.length; // How much each winner gets
-      for(uint256 j = 0; j < count; j++){
-         if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
-         winners[j].transfer(winnerEtherAmount);
-      }
-   }
+    function kill() public {
+        if (msg.sender == owner) 
+            selfdestruct(owner);
+
+    }
+
+    function checkPlayerExists(address player) public constant returns(bool) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i] == player) 
+                return true;
+        }
+        return false;
+
+    }
+
+    // To bet for a number between 1 and 10 both inclusive
+    function bet(uint256 numberSelected) public payable {
+        require(!checkPlayerExists(msg.sender));
+        require(numberSelected <= 10 && numberSelected >= 1);
+        require(msg.value >= minimumBet);
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].numberSelected = numberSelected;
+        numberOfBets++;
+        players.push(msg.sender);
+        totalBet += msg.value;
+        if (numberOfBets >= maximumBetsNr) 
+            generateNumberWinner();
+        //We need to change this in order to be secure
+
+    }
+
+    // Generates a number between 1 and 10 that will be the winner
+    function generateNumberWinner() public {
+        uint256 numberGenerated = block.number % 10 + 1;
+
+        distributePrizes(numberGenerated);
+
+    }
+
+    // Sends the corresponding ether to each winner depending on the total bets
+    function distributePrizes(uint256 numberWinner) public {
+        address[100] memory winners;
+        uint256 count = 0;
+        for (uint256 i = 0; i < players.length; i++) {
+            address playerAddress = players[i];
+            if (playerInfo[playerAddress].numberSelected == numberWinner) {
+                winners[count] = playerAddress;
+                count++;
+            }
+            delete playerInfo[playerAddress];
+        }
+
+        players.length = 0;
+        uint256 winnerEtherAmount = totalBet/winners.length;
+
+        for (uint256 j = 0; j < winners.length; j++){
+            if (winners[j] != address(0)) 
+                winners[j].transfer(winnerEtherAmount);
+        }
+
+        resetData();
+
+    }
+
+    function resetData() public {
+        players.length = 0;
+        totalBet = 0;
+        numberOfBets = 0;
+    }
+
 }
